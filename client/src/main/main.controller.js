cf367481b0be301b352bc0c645b940f42441aec2
Aaron Gunderson <aaron@agundy.com>
2015-10-12 21:37:45 -0400

    Added get by hash

--- client/src/main/main.controller.js
+++ client/src/main/main.controller.js
@@ -1,10 +1,15 @@
 'use strict';
 
-app.controller('MainCtrl', function($scope, ExplorerService, ConversionService){
+app.controller('MainCtrl', function($scope, $location, ExplorerService, ConversionService){
   $scope.ConvSrvc = ConversionService;
 
   ExplorerService.getStatus()
       .success(function(data) {
           $scope.status = data;
       });
+  $scope.getBlockByHash = function(){
+      if ($scope.blockHash !== ''){
+          $location.path('/block/' + $scope.blockHash);
+      }
+  }
 });
